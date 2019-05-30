b8e17b32e7c832f9982a49a150d63e4d58c3d411
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-05-30 14:37:18 +0200

    Adding elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,28 +1,28 @@
-//event handler
-function submitComment () {
-    //gathering the Elements
-    const inputField = document.getElementById("name");
-    const name = inputField.value;
-    const textArea = document.getElementById('msg');
-    const msg = textArea.value;
-    //creating elements we need
+// add event handler
+function submitComment() {
+    // gather data
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    
+    // create the elements 
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-    //adjusting elements created accordingly
-    h3.innerHTML = `${name} said:`;
-    p.innerHTML = msg;
-    comment.classList.add('comment');
-    comment.appendChild(h3);
-    comment.appendChild(p);
-    //diplaying the comments;
-    const commentSection = document.getElementById('comments');
-    commentSection.appendChild(comment);
-    //Reassigning input and textArea values;
-    inputField.value = null;
-    textArea.value = null;
-    if(!name || !msg){
-        alert('You did not put in a name or message')
-        return null
-    }  
-}
+  
+    // adjusting the elements 
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+  
+    // displaying the elements
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+  
+    // reseting form values
+    inputField.value = null
+    textArea.value = null
+  }
